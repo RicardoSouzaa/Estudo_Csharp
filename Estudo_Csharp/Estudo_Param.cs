00098f7fbc4337a8abfa753ea8f57a2a167d9ee2
Ricardo Mubarak <psycho696@gmail.com>
2021-03-18 20:26:26 -0300

    Adicionar arquivos de projeto.

---
+++ Estudo_Csharp/Estudo_Param.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Estudo_Csharp
+{
+    class Estudo_Param
+    {
+        static void estudo()
+        {
+           
+        }
+        public static void Somatoria(params int[] n) // criando um metodo somatoria com parametros de array com valores inteiros e nome = n
+        {
+            int res = 0;
+            if (n.Length < 1)
+            {
+                Console.WriteLine("Não existe valores para serem somados!");
+            }
+            else if (n.Length < 2)
+            {
+                Console.WriteLine($"Para realizar a somatória, precisa de dois ou mais numeros, foi digtado apenas o valor {n[0]}");
+            }
+            else
+            {
+                for (int i=0; i < n.Length; i++)
+                {
+                    res += n[i]; // res vai receber o incremento do valor de i (0+i=0), (0+i=1), (1+i) etc.. 
+                }
+                Console.WriteLine($"A somatória dos valores digitado é: {res}");
+            }
+        }
+    }
+
+}
---
+++ Estudo_Csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudo_Param.Somatoria(200,100, 10, 27, 88, 9, 16);
+            Console.ReadLine();
+        }
+    }
+}
